46a4e24e6b34f7d4827f7fa6da75cd3e0884aa99
G. Kay Lee <balancetraveller+github@gmail.com>
2015-02-26 18:44:23 +0800

    Add minified version

---
+++ gulptasks.js
@@ -0,0 +1,15 @@
+import Gulp from 'gulp';
+import $ from 'gulp-load-plugins';
+
+$ = $();
+
+Gulp.task('build:scripts',
+  () => Gulp.src('es6-traits.js')
+            .pipe($.rename('es6-traits.min.js'))
+            .pipe($.babel({
+              loose: 'all',
+              experimental: true
+            }))
+            .pipe($.uglify())
+            .pipe(Gulp.dest('.'))
+);
